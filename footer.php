c0b9d65836e2720429cf721b1639b40447d9f935
manikantaswamygangumalla <148608526+manikantaswamygangumalla@users.noreply.github.com>
2023-11-18 10:00:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> grocerystore@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> konaseema, india - 533238 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.youtube.com/@Andhra_food_vlogs"> <i class="fab fa-youtube"></i> Youtube </a>
+         <a href="https://instagram.com/andhra_food_vlogs?igshid=dXp2ZGtremJkaGtw"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/gangumalla-manikantaswamy-a27a96247?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Organic Store</span> | all rights reserved! </p>
+
+</footer>
